c66ae96cd4b0820e8a3f17c5d3a88240c3cd41c2
mottobotpot <mottobotpot@gmail.com>
2020-07-31 14:23:59 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-rwz8hx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-rwz8hx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: "my-app",
+  templateUrl: `./app.component.html`
+})
+export class AppComponent {
+  user = {
+    mail: '',
+    passwd: '',
+    name: '',
+  };
+  idtype = 'mail';
+  data = [
+    { label: 'メールアドレス', value: 'mail' },
+    { label: 'ID', value: 'id' }
+  ];
+  roll = '0';
+  data2 = [
+    { label: '日本語', value: '0'},
+    { label: '英語', value: '1'},
+    { label: '中国語', value: '2'}
+  ];
+  data3 = [
+    { label: 'パスワードを保存', value: 'checkbox', selected: false }
+  ];
+  show() {
+    if(this.data3[0].selected==true){
+      alert('ログインID：' + this.user.mail + '\n'
+      + 'IDの種類：' + this.data + '\n'
+      + 'パスワード：' + this.user.passwd + '\n'
+      + '言語：' + this.data2 + '\n'
+      + 'パスワードを保存：保存する\n'
+      );
+    }else{
+      alert('ログインID：' + this.user.mail + '\n'
+      + 'IDの種類：' + this.data + '\n'
+      + 'パスワード：' + this.user.passwd + '\n'
+      + '言語：' + this.data2 + '\n'
+      + 'パスワードを保存：保存しない\n'
+      );
+    }
+  }
+}
