b3ecf2d6588073bf6957c8c0639469419821fcbb
mottobotpot <mottobotpot@gmail.com>
2020-07-31 15:57:56 +0900

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ export class AppComponent {
     passwd: '',
     name: '',
   };
-  idtype = 'mail';
+  idtype = '0';
   data = [
-    { label: 'メールアドレス', value: 'mail' },
-    { label: 'ID', value: 'id' }
+    { label: 'メールアドレス', value: '0' },
+    { label: 'ID', value: '1' }
   ];
   roll = '0';
   data2 = [
@@ -28,16 +28,16 @@ export class AppComponent {
   show() {
     if(this.data3[0].selected==true){
       alert('ログインID：' + this.user.mail + '\n'
-      + 'IDの種類：' + this.idtype + '\n'
+      + 'IDの種類：' + this.data[this.idtype].label + '\n'
       + 'パスワード：' + this.user.passwd + '\n'
-      + '言語：' + this.roll + '\n'
+      + '言語：' + this.data2[this.roll].label + '\n'
       + 'パスワードを保存：保存する\n'
       );
     }else{
       alert('ログインID：' + this.user.mail + '\n'
-      + 'IDの種類：' + this.idtype + '\n'
+      + 'IDの種類：' + this.data[this.idtype].label + '\n'
       + 'パスワード：' + this.user.passwd + '\n'
-      + '言語：' + this.roll + '\n'
+      + '言語：' + this.data2[this.roll].label + '\n'
       + 'パスワードを保存：保存しない\n'
       );
     }
